e2ff6087b598993c70798da289e9e54016e75d28
Arun Kumar B <arunkumar@betamonks.com>
2025-08-05 12:18:13 +0530

    Fix  config

---
+++ src/main/java/com/zos/config/SecurityConfig.java
@@ -0,0 +1,22 @@
+package com.zos.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+            .cors(withDefaults())
+            .csrf(csrf -> csrf.disable())
+            .authorizeHttpRequests(auth -> auth
+                .anyRequest().permitAll()
+            );
+        return http.build();
+    }
+}
