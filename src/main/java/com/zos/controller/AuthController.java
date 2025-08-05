dc2eb6813edd449946a7a4f4a9e5e3dce8a8b357
Arun Kumar B <arunkumar@betamonks.com>
2025-08-05 11:23:17 +0530

    Add CORS support to AuthController

--- src/main/java/com/zos/controller/AuthController.java
+++ src/main/java/com/zos/controller/AuthController.java
@@ -11,6 +11,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,6 +29,7 @@ import jakarta.validation.Valid;
 
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping("/auth")
 public class AuthController {
 	
